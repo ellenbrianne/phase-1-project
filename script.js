33d18e955f50b24c538a4f4ec029c95152a0bd04
Ellen Bennett <bennette831@gmail.com>
2023-05-13 16:30:00 -0600

    dropdowns for initial list, working mouseenter event

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let title = document.getElementById("title")
+let hidden = document.getElementById("hide")
+title.addEventListener("mouseenter", (e) => {
+    hidden.id = ""
+    setTimeout(() => {
+        hidden.id = "hide"
+    }, 2000)
+})
+
 const form1 = document.getElementById("text-input")
 
 form1.addEventListener("submit", (e) => {
@@ -5,16 +14,24 @@ form1.addEventListener("submit", (e) => {
     let input = form1.querySelector("#habit").value
     let newItem = document.createElement("li")
     let list = document.querySelector("ul").appendChild(newItem)
-    newItem.textContent = input
+    newItem.innerHTML = `
+        ${input}
+        <select name="rank" id="drop-btn">
+            <option value="empty"></option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <br></br>
+        `
     
     form1.reset()
 })
 
-
-
 function renderCard (habit) {
-    let card = document.createElement("div")
-    card.className = "card"
+    let card = document.createElement("p")
+    card.className = "cards"
+    card.id = `${habit.id}`
 
     card.innerHTML = `
         <h3>${habit.activity}</h3>
@@ -24,6 +41,11 @@ function renderCard (habit) {
 
     document.querySelector(".prompts").appendChild(card)
 }
+
+let promptBtn = document.getElementById("prompt-button")
+promptBtn.addEventListener("click", (e) => {
+   getHabits() 
+})
 
 function getHabits () {
     fetch("http://localhost:3000/habits")
